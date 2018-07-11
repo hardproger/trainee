24dc5e3baf83c95529de1dc3a7694c9b9942fee8
Vladislav Pereguda <vpereguda@freshcode.org>
2018-07-11 15:46:20 +0300

    slider & home page was made

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { UserService } from './services/user.service';
+import { AuthService } from './services/auth.services';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +12,17 @@ import { UserService } from './services/user.service';
 })
 export class AppComponent implements OnInit {
   constructor(private userService: UserService,
-              private router: Router) {}
+              private router: Router,
+              private auth: AuthService) {}
   ngOnInit() {
     console.log('onInit');
     this.userService.checkLogin().subscribe(
       () => {},
       () => {
         localStorage.removeItem('token');
+        this.auth.currentUser = new User();
+        this.auth.loggedIn = false;
+        this.auth.isAdmin = false;
         this.router.navigate(['/']);
       }
     );
--- client/app/start/start.component.ts
+++ client/app/start/start.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.services';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-home',
   templateUrl: './start.component.html',
   styleUrls: ['./start.component.css']
 })
-export class StartComponent {
+export class StartComponent implements OnInit{
   constructor(private auth: AuthService) {}
+  ngOnInit() {
+    // $('.carousel .carousel-item').each(function(){
+    //   let next = $(this).next();
+    //   if (!next.length) {
+    //     next = $(this).siblings(':first');
+    //   }
+    //   next.children(':first-child').clone().appendTo($(this));
+    //   for (let i = 0 ; i < 1 ; i++) {
+    //     next = next.next();
+    //     if (!next.length) {
+    //       next = $(this).siblings(':first');
+    //     }
+    //     next.children(':first-child').clone().appendTo($(this));
+    //   }
+    // });
+  }
 }
